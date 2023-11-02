34bb3717f193ea6f431f3a7b2ea95e883f0885d3
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-02 17:22:09 -0500

    update resposive

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
             <section className="relative">
                 <div className="relative pt-1 pb-32 flex content-center items-center justify-center min-h-screen-75">
                     <div className="absolute top-0 w-full h-full bg-center bg-cover" >
-                    <span id="blackOverlay" className="w-full h-full absolute  bg-dark"></span>
+                    <span id="blackOverlay" className="w-full h-full absolute bg-dark"></span>
                     </div>
                     <div className="container relative mx-auto">
                     <div className="items-center flex flex-wrap">
@@ -22,9 +22,6 @@ const Intro = () => {
                     </div>
                     </div>
                     <div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px">
-                    <svg className="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-                        <polygon className="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
-                    </svg>
                     </div>
                 </div>
                 <section className="pb-10 bg-blueGray-200 -mt-24">
