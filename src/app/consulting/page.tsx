c756a4373f93e332491da2c14e4b2a9eff667b93
Willians Jimenez <williansaaid@hotmail.com>
2023-12-04 21:42:14 -0500

    sticky section

--- src/app/consulting/page.tsx
+++ src/app/consulting/page.tsx
@@ -11,10 +11,10 @@ export default function Consulting() {
                 <meta name="description" content="Asesoría personalizada de parte de Devrise"/>
             </Head>
             <main
-                className="flex items-center flex-col min-h-screen h-fit w-full bg-dark"
+                className="flex items-center flex-col min-h-screen h-fit w-full bg-dark relative"
             >
                 <section
-                    className="flex flex-col md:flex-row justify-center items-center w-full h-[calc(100vh-100px)]"
+                    className="flex flex-col md:flex-row justify-center items-center w-full h-[calc(100vh-100px)] sticky top-[100px]"
                 >
                     <div className="w-full md:w-2/3 justify-center items-center bg-code bg-blur-xs bg-center bg-cover h-[calc(100%)]">
                         <div
@@ -48,6 +48,80 @@ export default function Consulting() {
                             sizes="(min-width: 768px) 33.3vw, (max-width: 768px) 100vw"
                             priority draggable={false}
                         />
+                    </div>
+                </section>
+                <section
+                    className="flex flex-col md:flex-row justify-center items-center w-full h-[calc(100vh-100px)] sticky top-[100px]"
+                >
+                    <div
+                        className='flex items-center justify-center w-full md:w-1/3 h-[calc(100%)] relative overflow-hidden'
+                    >
+                        <Image
+                            src='https://res.cloudinary.com/ds41xxspf/image/upload/v1700699777/DevRise/pexels-thisisengineering-3861959_a083my.jpg'
+                            alt="Programador"
+                            fill
+                            className='w-full h-full object-cover'
+                            sizes="(min-width: 768px) 33.3vw, (max-width: 768px) 100vw"
+                            priority draggable={false}
+                        />
+                    </div>
+                    <div className="w-full md:w-2/3 justify-center items-center bg-code bg-blur-xs bg-center bg-cover h-[calc(100%)]">
+                        <div
+                            className="backdrop-blur-sm h-full flex flex-col justify-center items-center p-8"
+                        >
+                            <AnimatedText
+                                text="Impulsando Tu Presencia Digital"
+                                fromTop
+                                className="font-bold text-center text-4xl sm:text-5xl md:text-6xl xl:text-7xl text-yellow"
+                                stylesWords="hover:text-white"
+                            />
+                            <AnimatedDiv
+                                className="flex justify-center items-center"
+                            >
+                                <p
+                                    className="text-center text-xl sm:text-2xl md:text-3xl xl:text-4xl"
+                                >
+                                    En DEVRISE, entendemos que la clave para el éxito en línea va más allá de la simple presencia web.
+                                </p>
+                            </AnimatedDiv>
+                        </div>
+                    </div>
+                </section>
+                <section
+                    className="flex flex-col md:flex-row justify-center items-center w-full h-[calc(100vh-100px)] sticky top-[100px]"
+                >
+                    <div
+                        className='flex items-center justify-center w-full md:w-1/3 h-[calc(100%)] relative overflow-hidden'
+                    >
+                        <Image
+                            src='https://res.cloudinary.com/ds41xxspf/image/upload/v1700699777/DevRise/pexels-thisisengineering-3861959_a083my.jpg'
+                            alt="Programador"
+                            fill
+                            className='w-full h-full object-cover'
+                            sizes="(min-width: 768px) 33.3vw, (max-width: 768px) 100vw"
+                            priority draggable={false}
+                        />
+                    </div>
+                    <div className="w-full md:w-2/3 justify-center items-center bg-code bg-blur-xs bg-center bg-cover h-[calc(100%)]">
+                        <div
+                            className="backdrop-blur-sm h-full flex flex-col justify-center items-center p-8"
+                        >
+                            <AnimatedText
+                                text="Impulsando Tu Presencia Digital"
+                                fromTop
+                                className="font-bold text-center text-4xl sm:text-5xl md:text-6xl xl:text-7xl text-yellow"
+                                stylesWords="hover:text-white"
+                            />
+                            <AnimatedDiv
+                                className="flex justify-center items-center"
+                            >
+                                <p
+                                    className="text-center text-xl sm:text-2xl md:text-3xl xl:text-4xl"
+                                >
+                                    En DEVRISE, entendemos que la clave para el éxito en línea va más allá de la simple presencia web.
+                                </p>
+                            </AnimatedDiv>
+                        </div>
                     </div>
                 </section>
             </main>
